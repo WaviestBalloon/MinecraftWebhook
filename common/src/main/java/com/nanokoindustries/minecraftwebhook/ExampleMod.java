862a48c6a14b8923e2e56cd360c36540b2711f52
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2024-10-09 11:45:19 +0100

    Inital

---
+++ common/src/main/java/com/nanokoindustries/minecraftwebhook/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.nanokoindustries.minecraftwebhook;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "minecraftwebhook";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
